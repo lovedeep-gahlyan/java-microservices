091ed5bac42da7b76557d16b554887ccd28a7ac4
lovedeep <08lovedeep@gmail.com>
2024-06-27 16:20:58 +0530

    conifguration management

---
+++ v1/account/src/main/java/com/bank/account/AccountApplication.java
@@ -0,0 +1,45 @@
+package com.bank.account;
+
+import com.bank.account.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice Rest API Documentation",
+				description = "Bank Accounts Microservice api documentaion",
+				version = "v1",
+				contact = @Contact(
+						name = "Lovedeep",
+						email = "lovedeep@gmail.com",
+						url = "https//www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https//www.google.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Accounts microservice documentation"
+		)
+)
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
